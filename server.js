cc4e480e4148b33535ef328faf1fe6cbfe686b32
Hirwa Clement Rhodin <h1rhodin@gmail.com>
2025-06-19 14:50:26 +0200

    changes on server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/user/:id', (req, res) => {
 });
 
 // Fallback error handler
-app.use((err, res) => {
+app.use((err, req, res, next) => { 
   console.error(err.stack);
   res.status(500).json({ error: 'Internal Server Error' });
 });
